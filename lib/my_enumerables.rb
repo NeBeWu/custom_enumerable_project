cb9489d0e297da9e9e783a58807fb7b8f8845a5b
Pedro <nebewu@gmail.com>
2022-04-03 01:45:10 -0300

    Implement all methods passing all tests.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,111 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+
+    self
+  end
+
+  def my_select
+    i = 0
+    result = []
+
+    while i < self.length
+      result << self[i] if yield(self[i])
+      i += 1
+    end
+
+    result
+  end
+
+  def my_all?
+    i = 0
+    result = true
+
+    while i < self.length && result
+      result = yield(self[i])
+      i += 1
+    end
+
+    result
+  end
+
+  def my_any?
+    i = 0
+
+    while i < self.length
+      return yield(self[i]) if yield(self[i])
+      i += 1
+    end
+
+    false
+  end
+
+  def my_none?
+    i = 0
+    result = true
+
+    while i < self.length && result
+      result = !yield(self[i])
+      i += 1
+    end
+
+    result
+  end
+
+  def my_count
+    return self.length unless block_given?
+
+    i = 0
+    result = 0
+
+    while i < self.length
+      result += 1 if yield(self[i])
+      i += 1
+    end
+
+    result
+  end
+
+  def my_map
+    i = 0
+    result = []
+
+    while i < self.length
+      result << yield(self[i])
+      i += 1
+    end
+
+    result
+  end
+
+  def my_inject(initial_value = nil)
+    i = 0
+    result = initial_value unless initial_value.nil?
+
+    while i < self.length && result
+      result = yield(result, self[i])
+      i += 1
+    end
+
+    result
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+
+    self
+  end
 end
+
